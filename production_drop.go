2fda0e577e77e0e2511b0e1c16c542d6d1426888
Oliver Jakoubek <info@jakoubek.net>
2020-07-09 11:41:49 +0200

    Added comments to Production Drop. Fixes #10

--- production_drop.go
+++ production_drop.go
@@ -2,6 +2,8 @@ package feragstring
 
 import "fmt"
 
+// ProductionDrop is the struct for one production drop
+// underneath a route
 type ProductionDrop struct {
 	FeragMessage
 	agentName         string
@@ -11,6 +13,7 @@ type ProductionDrop struct {
 	topsheetData      string
 }
 
+// TopsheetData returns the formatted topsheet data segment
 func (pd *ProductionDrop) TopsheetData() string {
 	if pd.topsheetData == "" {
 		return ""
@@ -30,6 +33,7 @@ func (pd *ProductionDrop) TopsheetData() string {
 	return message(&fm, tsdSegment)
 }
 
+// SetTopsheetData sets the topsheet data to a given string
 func (pd *ProductionDrop) SetTopsheetData(topsheetData string) {
 	pd.topsheetData = topsheetData
 }
@@ -38,22 +42,28 @@ func (pd *ProductionDrop) SetDontProduce() {
 	pd.dontProduce = true
 }
 
+// NumberOfCopies returns the formatted number of copies in the route
 func (pd *ProductionDrop) NumberOfCopies() string {
 	return fmt.Sprintf("+13%05d", pd.numberOfCopies)
 }
 
+// SetNumberOfCopies sets the number of copies in the production drop
 func (pd *ProductionDrop) SetNumberOfCopies(numberOfCopies int) {
 	pd.numberOfCopies = numberOfCopies
 }
 
+// AgentName returns the formatted agent name
 func (pd *ProductionDrop) AgentName() string {
 	return fmt.Sprintf("+12%-10s", pd.agentName)
 }
 
+// SetAgentName sets the agent name to a given string
 func (pd *ProductionDrop) SetAgentName(agentName string) {
 	pd.agentName = agentName
 }
 
+// NewProductionDrop instantiates a new production drop
+// struct and returns a pointer to it.
 func NewProductionDrop() *ProductionDrop {
 	pd := ProductionDrop{
 		FeragMessage: FeragMessage{
@@ -65,6 +75,8 @@ func NewProductionDrop() *ProductionDrop {
 	return &pd
 }
 
+// Payload returns the formatted FERAG string
+// for embedding in the message
 func (pd *ProductionDrop) Payload() string {
 	data := pd.AgentName()
 	data += pd.NumberOfCopies()
@@ -72,6 +84,8 @@ func (pd *ProductionDrop) Payload() string {
 	return data
 }
 
+// Message returns the formatted FERAG string
+// for the production drop
 func (pd *ProductionDrop) Message() string {
 	message := pd.FeragMessage.MessageTemplate()
 	return message(&pd.FeragMessage, pd.Payload())
